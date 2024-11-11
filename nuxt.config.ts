f9a55ef33c11b12771645c9c10ebca42ab726126
Mehmet Uysal <siliente2020@gmail.com>
2024-11-11 16:44:11 +0300

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
+  sourcemap: false,
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
 });
